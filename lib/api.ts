9354e8e8eff1d3754f05798a29d4ac4bc736571b
talles <tallessuares48@gmail.com>
2026-07-20 15:52:32 -0300

    Adjustments to refreshToken in api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,11 +1,20 @@
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3333";
 
+async function refresh() {
+    const response = await fetch(`${API_URL}/auth/refresh`, {
+        method: "POST",
+        credentials: "include"
+    })
+
+    return response.ok
+}
+
 export async function api<T>(
     endpoint: string,
     options?: RequestInit,
 ) {
 
-    const res = await fetch(`${API_URL}${endpoint}`, {
+    const request = () => fetch(`${API_URL}${endpoint}`, {
         ...options,
         credentials: "include",
         headers: {
@@ -15,10 +24,20 @@ export async function api<T>(
         ...options
     })
 
-    if (!res.ok) {
-        const error = await res.json().catch(() => ({}))
+    let response = await request()
+
+    if(response.status === 401 && endpoint !== "/auth/refresh" && endpoint !== "/login") {
+        const refreshed = await refresh()
+
+        if(refreshed) {
+            await request()
+        }
+    }
+
+    if (!response.ok) {
+        const error = await response.json().catch(() => ({}))
         throw new Error(error.message || "Request failed")
     }
 
-    return {data: await res.json(), ok: res.ok}
+    return {data: await response.json(), ok: true}
 }
